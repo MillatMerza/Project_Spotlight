4a31542abfcdec4940073b1c9c26166db34c43a2
Millat <millatmzere72753@gmail.com>
2025-03-02 02:02:09 +0300

    testing_day 7

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", function () {
       setTimeout(() => {
         loadingScreen.style.display = "none";
       }, 500);
-    }, 2000);
+    }, 4000);
   }
 
   // Navbar Toggle (immediately executed)
